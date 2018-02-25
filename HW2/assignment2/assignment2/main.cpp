742a9eb13e7895b0a4edc286ddbf7adbbf8b12bd
Natalia <ndibbern@lion.lmu.edu>
2018-02-24 16:53:59 -0800

    the thing from before was the tutorial not the skeleton, upload skeleton now

--- HW2/assignment2/assignment2/main.cpp
+++ HW2/assignment2/assignment2/main.cpp
@@ -1,10 +1,17 @@
-//
-//  main.cpp
-//  cube-tutorial
-//
-//  Created by Alex Wong on 2/11/18.
-//  Copyright © 2018 Alex Wong. All rights reserved.
-//
+/***
+ Assignment-2: Geometric Modeling of a Scene
+ 
+ Name: Wong, Alex (Please write your name in Last Name, First Name format)
+ 
+ Collaborators: Doe, John; Doe, Jane
+ ** Note: although the assignment should be completed individually
+ you may speak with classmates on high level algorithmic concepts. Please
+ list their names in this section
+ 
+ Project Summary: A short paragraph (3-4 sentences) describing the work you
+ did for the project.
+ ***/
+
 
 
 #ifdef __APPLE__
@@ -17,7 +24,121 @@
 #include <GL/glut.h>
 #endif
 
-float theta = 0.0;
+#include <math.h>
+#include <vector>
+using namespace std;
+
+/**************************************************
+ *  Rectangular Prisms via Hierarchical Modeling  *
+ *                                                *
+ *  using planes as building blocks, build a unit *
+ *  cube with transformations that will serve as  *
+ *  a primitive for modeling objects in the scene *
+ *                                                *
+ *************************************************/
+
+// Initializes a square plane of unit lengths
+vector<GLfloat> init_plane() {
+    vector<GLfloat> vertices = {
+        +0.5,   +0.5,   +0.0,
+        -0.5,   +0.5,   +0.0,
+        -0.5,   -0.5,   +0.0,
+        +0.5,   -0.5,   +0.0
+    };
+    return vertices;
+}
+
+// Converts a vector to an array
+GLfloat* vector2array(vector<GLfloat> vec) {
+    GLfloat* arr = new GLfloat[vec.size()];
+    for (int i = 0; i < vec.size(); i++) {
+        arr[i] = vec[i];
+    }
+    return arr;
+}
+
+// Converts Cartesian coordinates to homogeneous coordinates
+vector<GLfloat> to_homogenous_coord(vector<GLfloat> cartesian_coords) {
+    vector<GLfloat> result;
+    
+    // Append the 1 in the 4th dimension to generate homoegenous coordinates
+    
+    return result;
+}
+
+// Converts Cartesian coordinates to homogeneous coordinates
+vector<GLfloat> to_cartesian_coord(vector<GLfloat> homogenous_coords) {
+    vector<GLfloat> result;
+    
+    // Remove the 1 in the 4th dimension to generate Cartesian coordinates
+    
+    return result;
+}
+
+// Definition of a translation matrix
+vector<GLfloat> translation_matrix (float dx, float dy, float dz) {
+    vector<GLfloat> translate_mat;
+    
+    return translate_mat;
+}
+
+// Definition of a scaling matrix
+vector<GLfloat> scaling_matrix (float sx, float sy, float sz) {
+    vector<GLfloat> scale_mat;
+    
+    return scale_mat;
+}
+
+// Definition of a rotation matrix along the x-axis theta degrees
+vector<GLfloat> rotation_matrix_x (float theta) {
+    vector<GLfloat> rotate_mat_x;
+    
+    return rotate_mat_x;
+}
+
+
+// Definition of a rotation matrix along the y-axis by theta degrees
+vector<GLfloat> rotation_matrix_y (float theta) {
+    vector<GLfloat> rotate_mat_y;
+    
+    return rotate_mat_y;
+}
+
+
+// Definition of a rotation matrix along the z-axis by theta degrees
+vector<GLfloat> rotation_matrix_z (float theta) {
+    vector<GLfloat> rotate_mat_z;
+    
+    return rotate_mat_z;
+}
+
+// Perform matrix multiplication for A B
+vector<GLfloat> mat_mult(vector<GLfloat> A, vector<GLfloat> B) {
+    vector<GLfloat> result;
+    
+    // Perform matrix multiplication for A B
+    
+    return result;
+}
+
+// Builds a unit cube centered at the origin
+vector<GLfloat> build_cube() {
+    vector<GLfloat> result;
+    
+    // Creates a unit cube by transforming a set of planes
+    
+    return result;
+}
+
+/**************************************************
+ *            Camera and World Modeling           *
+ *                                                *
+ *  create a scene by applying transformations to *
+ *  the objects built from planes and position    *
+ *  the camera to view the scene by using setting *
+ *  the projection viewing matrices               *
+ *                                                *
+ *************************************************/
 
 void setup() {
     // Enable the vertex array functionality
@@ -38,110 +159,27 @@ void init_camera() {
     // Camera parameters
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    // Define a 50 degree field of view, 1:1 aspect ratio, near and far planes at 3 and 7
-    gluPerspective(50.0, 1.0, 2.0, 10.0);
-    // Position camera at (2, 3, 5), attention at (0, 0, 0), up at (0, 1, 0)
-    gluLookAt(2.0, 6.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+}
+
+// Construct the scene using objects built from cubes/prisms
+GLfloat* init_scene() {
+    return nullptr;
+}
+
+// Construct the color mapping of the scene
+GLfloat* init_color() {
+    return nullptr;
 }
 
 void display_func() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     
-    // World model parameters
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    
-    glRotatef(theta, 0.0, 1.0, 0.0);
-    glRotatef(theta, 1.0, 0.0, 0.0);
-    
-    GLfloat vertices[] = {
-        // Front plane
-        +1.0,   +1.0,   +1.0,
-        -1.0,   +1.0,   +1.0,
-        -1.0,   -1.0,   +1.0,
-        +1.0,   -1.0,   +1.0,
-        // Back plane
-        +1.0,   +1.0,   -1.0,
-        -1.0,   +1.0,   -1.0,
-        -1.0,   -1.0,   -1.0,
-        +1.0,   -1.0,   -1.0,
-        // Right
-        +1.0,   +1.0,   -1.0,
-        +1.0,   +1.0,   +1.0,
-        +1.0,   -1.0,   +1.0,
-        +1.0,   -1.0,   -1.0,
-        // Left
-        -1.0,   +1.0,   -1.0,
-        -1.0,   +1.0,   +1.0,
-        -1.0,   -1.0,   +1.0,
-        -1.0,   -1.0,   -1.0,
-        // Top
-        +1.0,   +1.0,   +1.0,
-        -1.0,   +1.0,   +1.0,
-        -1.0,   +1.0,   -1.0,
-        +1.0,   +1.0,   -1.0,
-        // Bottom
-        +1.0,   -1.0,   +1.0,
-        -1.0,   -1.0,   +1.0,
-        -1.0,   -1.0,   -1.0,
-        +1.0,   -1.0,   -1.0,
-    };
-    
-    GLfloat colors[] = {
-        // Front plane
-        1.0,    0.0,    0.0,
-        1.0,    0.0,    0.0,
-        1.0,    0.0,    0.0,
-        1.0,    0.0,    0.0,
-        // Back plane
-        0.0,    1.0,    0.0,
-        0.0,    1.0,    0.0,
-        0.0,    1.0,    0.0,
-        0.0,    1.0,    0.0,
-        // Right
-        0.0,    0.0,    1.0,
-        0.0,    0.0,    1.0,
-        0.0,    0.0,    1.0,
-        0.0,    0.0,    1.0,
-        // Left
-        1.0,    1.0,    0.0,
-        1.0,    1.0,    0.0,
-        1.0,    1.0,    0.0,
-        1.0,    1.0,    0.0,
-        // Top
-        1.0,    0.0,    1.0,
-        1.0,    0.0,    1.0,
-        1.0,    0.0,    1.0,
-        1.0,    0.0,    1.0,
-        // Bottom
-        0.0,    1.0,    1.0,
-        0.0,    1.0,    1.0,
-        0.0,    1.0,    1.0,
-        0.0,    1.0,    1.0,
-    };
-    
-    glVertexPointer(3,          // 3 components (x, y, z)
-                    GL_FLOAT,   // Vertex type is GL_FLOAT
-                    0,          // Start position in referenced memory
-                    vertices);  // Pointer to memory location to read from
-    
-    //pass the color pointer
-    glColorPointer(3,           // 3 components (r, g, b)
-                   GL_FLOAT,    // Vertex type is GL_FLOAT
-                   0,           // Start position in referenced memory
-                   colors);     // Pointer to memory location to read from
-    
-    // Draw quad point planes: each 4 vertices
-    glDrawArrays(GL_QUADS, 0, 4*6);
+    // Perform display functions
     
     glFlush();            //Finish rendering
     glutSwapBuffers();
 }
 
-void idle_func() {
-    theta = theta+0.3;
-    display_func();
-}
 
 int main (int argc, char **argv) {
     // Initialize GLUT
@@ -149,16 +187,20 @@ int main (int argc, char **argv) {
     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
     glutInitWindowSize(800, 600);
     // Create a window with rendering context and everything else we need
-    glutCreateWindow("Vertex Arrays");
+    glutCreateWindow("Assignment 2");
     
     setup();
     init_camera();
     
     // Set up our display function
     glutDisplayFunc(display_func);
-    glutIdleFunc(idle_func);
     // Render our world
     glutMainLoop();
+    
+    // Remember to call "delete" on your dynmically allocated arrays
+    // such that you don't suffer from memory leaks. e.g.
+    // delete arr;
+    
     return 0;
 }
 
